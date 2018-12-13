86cb842ed46ddff4c12266ed34cbac166786fdb7
netherSAYnether <npeel3497@gmail.com>
2018-12-13 13:23:11 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package org.duberapp.dubertest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the numbers category
+        Button signin = findViewById(R.id.signin);
+
+        // Set a click listener on that View
+        signin.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent signin = new Intent(MainActivity.this, SigninActivity.class);
+
+                // Start the new activity
+                startActivity(signin);
+            }
+        });
+
+
+    }
+
+
+}
