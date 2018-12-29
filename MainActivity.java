069d356415a5bc2d530958f4148d2aef4034e42a
netherSAYnether <npeel3497@gmail.com>
2018-12-29 11:19:11 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,10 @@
 package org.duberapp.dubertest;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,24 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the View that shows the numbers category
-        Button signin = findViewById(R.id.signin);
+        Button mGrower = findViewById(R.id.grower);
+        Button mSmoker = findViewById(R.id.smoker);
 
-        // Set a click listener on that View
-        signin.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link NumbersActivity}
-                Intent signin = new Intent(MainActivity.this, SigninActivity.class);
 
-                // Start the new activity
-                startActivity(signin);
+        mGrower.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GrowerLoginActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
-
+        mSmoker.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SmokerLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
-
-
 }
